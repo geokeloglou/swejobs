80c28a51eab81c9691d89a99fc2e4d7be3ae7e49
geokeloglou <geokeloglou@gmail.com>
2019-12-18 19:55:37 +0200

    api /job response

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/jobs', (req, res) => {
-    res.send('Hello World!')
-});
+var redis = require("redis"),
+    client = redis.createClient();
+
+const {promisify} = require('util');
+const getAsync = promisify(client.get).bind(client);
+
+app.get('/jobs', async (req, res) => {
+    const jobs = await getAsync('github');
+    return res.send(jobs)
+})
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
