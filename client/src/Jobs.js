e290d7de27f2d1f9a785bc6a9c192da1a22b9573
geokeloglou <geokeloglou@gmail.com>
2019-12-18 22:26:22 +0200

    Update Jobs.js

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Job from './Job';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
-import Job from './Job';
 
 const useStyles = makeStyles({
     root: {
@@ -17,27 +17,29 @@ const useStyles = makeStyles({
 
 export default function Jobs({jobs}) {
 
+    // pagination
     const numJobs = jobs.length;
     const numPages = Math.ceil(numJobs / 50);
-
     const [activeStep, setActiveStep] = React.useState(0);
+    const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50);
+
     const classes = useStyles();
     const theme = useTheme();
 
-    const handleNext = () => {
-        setActiveStep(prevActiveStep =>   + 1);
-      };
-    
-      const handleBack = () => {
+    function handleNext() {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    }
+
+    function handleBack() {
         setActiveStep(prevActiveStep => prevActiveStep - 1);
-      };
+    }    
 
     return (
         <div className="jobs">
             <Typography variant="h4" component="h1">
                 Junior SWE Jobs
             </Typography>
-            { jobs.map((job, i) => <Job key={i} job={job} />) }
+            { jobsOnPage.map((job, i) => <Job key={i} job={job} />) }
             <div>Page {activeStep + 1} of {numPages}</div>
             <MobileStepper
                 variant="progress"
